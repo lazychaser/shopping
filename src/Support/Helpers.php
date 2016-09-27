cfa0b842cda895d264de31a392e0eaf89aafa75a
Aleksander Kalnoy <lazychaser@gmail.com>
2016-09-27 15:52:19 +0300

    Update

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -8,9 +8,9 @@ class Helpers
 {
     /**
      * Set properties on object using array of properties.
-     * 
+     *
      * It checks whether an object has a property setter and uses it first.
-     * 
+     *
      * @param object $object
      * @param array $properties
      */
@@ -18,9 +18,9 @@ class Helpers
     {
         foreach ($properties as $property => $value) {
             $property = Str::camel($property);
-            
+
             $method = 'set'.ucfirst($property);
-            
+
             if (method_exists($object, $method)) {
                 $object->$method($value);
             } else {
